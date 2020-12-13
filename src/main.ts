47b253acc245bca6acd951af59252718c9aae9ef
wirekang <wirekang@gmail.com>
2020-12-13 11:33:15 +0900

    First

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import Server from './Server';
+
+const flags = require('flags');
+
+flags.defineInteger('port', 80, 'port');
+flags.parse();
+const server = new Server(flags.get('port'));
+process.stdin.resume();
+
+function cleanup() {
+  console.log('Clean start.');
+  server.close();
+  console.log('Clean------');
+}
+
+function cleanAndExit() {
+  cleanup();
+  process.exit(0);
+}
+
+process.on('exit', cleanup);
+process.on('SIGINT', cleanAndExit);
+process.on('SIGTERM', cleanAndExit);
