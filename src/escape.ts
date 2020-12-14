cbf83be1ae51cb564dd14f126ab4f5e8bef28379
wirekang <wirekang@gmail.com>
2020-12-14 19:34:39 +0900

    Add quot escape

--- src/escape.ts
+++ src/escape.ts
@@ -5,7 +5,9 @@ export default {
     return str.replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
-      .replace(/\\n/g, '<br>');
+      .replace(/\\n/g, '<br>')
+      .replace(/\\"/g, '$quot;')
+      .replace(/\\'/g, '$apos;');
   },
   sql(str:string):string {
     return sqlstring.escape(str);
